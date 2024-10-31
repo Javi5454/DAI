23c7a6a8b92a7b8108fed8836ebb9b62d3f50856
Javi5454 <javier-gomezlopez@outlook.es>
2024-10-31 21:07:31 +0100

    Barra de bsuqueda hecha e inicio de vista de producto

--- P2/model/productos.js
+++ P2/model/productos.js
@@ -5,29 +5,29 @@ const ProductosSchema = new mongoose.Schema({
         "type": "Number",
         "unique": true
     },
-    "title" :{
-        "type" : "String",
+    "title": {
+        "type": "String",
         "required": true //Queremos que haya sí o sí
     },
-    "price":{
+    "price": {
         "type": "Number",
         "required": true //Queremos que haya sí o sí
     },
-    "description":{
+    "description": {
         "type": "String"
     },
-    "category":{
+    "category": {
         "type": "String"
     },
-    "image":{
+    "image": {
         "type": "String"
     },
-    "rating":{
-        "rate":{
+    "rating": {
+        "rate": {
             "type": "Number"
         },
-        "count":{
-            "type": "Number" 
+        "count": {
+            "type": "Number"
         }
     }
 });
@@ -36,13 +36,42 @@ const Productos = mongoose.model("productos", ProductosSchema);
 
 // Función para obtener los 3 artículos mejor valorados
 export async function obtenerTopProductos() {
-    try{
+    try {
         const topProductos = await Productos.find()
-            .sort({ "title" : -1}) //Orden descendente
+            .sort({ "title": -1 }) //Orden descendente
             .limit(3); //Solo los 3 mejores
         return topProductos;
-    } catch(err){
+    } catch (err) {
         console.error("Error al obtener los productos con mejor rate: ", err);
+    }
+}
+
+//Funcion para obtener los productos usando la barra de búsqueda
+export async function searchBarProductos(query) {
+    try {
+        const productos = await Productos.find({
+            $or: [
+                { "title": { $regex: query, $options: 'i' } },
+                { "description": { $regex: query, $options: 'i' } }
+            ]
+        });
+
+        return productos;
+    } catch (err) {
+        console.error("Error en la búsqueda de productos:", err);
+        throw err;
+    }
+}
+
+//Funcion para buscar un producto por ID
+export async function searchProductByID(producto_id) {
+    try{
+        const producto = await Productos.find({ "id" : producto_id});
+
+        return producto;
+    } catch (err){
+        console.log("Error al obtener el producto:", err);
+        throw err;
     }
 }
 
--- P2/routes/router_tienda.js
+++ P2/routes/router_tienda.js
@@ -1,6 +1,6 @@
 import express from "express";
 import Productos from "../model/productos.js";
-import { obtenerTopProductos } from "../model/productos.js";
+import { obtenerTopProductos, searchBarProductos, searchProductByID } from "../model/productos.js";
 const router = express.Router();
 
 router.get('/portada', async (req, res) => {
@@ -8,28 +8,50 @@ router.get('/portada', async (req, res) => {
         const categorias = await Productos.distinct('category') //Obtenemos las categorias
         const featured_articles = await obtenerTopProductos();
         res.render('portada.html', { categorias, featured_articles }); //../views/portada.html
-    }catch(err){
-        res.status(500).send({err})
+    } catch (err) {
+        res.status(500).send({ err })
     }
 })
 
 router.post('/portada', async (req, res) => {
     try {
         const categorias = await Productos.distinct('category') //Obtenemos las categorias
-        const featured_articles = await obtenerTopProductos();
-        console.log(req.body);
-        res.render('portada.html', { categorias, featured_articles }); //../views/portada.html
-    }catch(err){
-        res.status(500).send({err})
+        const titulo = req.body.query;
+        const search_result = await searchBarProductos(titulo);
+        console.log(search_result)
+        res.render('busqueda_productos.html', { categorias, search_result, titulo }); //../views/portada.html
+    } catch (err) {
+        res.status(500).send({ err })
+    }
+})
+
+//Vista de productos
+router.get('/producto/:id', async (req, res) => {
+    const productId = parseInt(req.params.id); //Tomamos el ID del producto de la URL
+
+    try {
+        //Buscamos el producto
+        let producto = await searchProductByID(productId);
+
+        if (producto.length <= 0) {
+            return res.status(404).send("Producto no encontrado");
+        }
+
+        producto = producto[0] //Invariante solo tomamos el primer producto
+
+        //Renderizamos la vista del producto
+        res.render('producto.html', { producto });
+    } catch (err){
+        res.status(500).send(err);
     }
 })
 
 
-router.get('/', async (req,res) => {
-    try{
+router.get('/', async (req, res) => {
+    try {
         res.render('home.html');
-    }catch(err){
-        res.status(500).send({err});
+    } catch (err) {
+        res.status(500).send({ err });
     }
 })
 
